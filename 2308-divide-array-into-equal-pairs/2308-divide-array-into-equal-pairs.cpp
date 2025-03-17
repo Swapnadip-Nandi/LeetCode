cb0deaf657dd9ea3a187eb3074fab9884683a95b
Swapnadip Nandi <125176423+Swapnadip-Nandi@users.noreply.github.com>
2025-03-17 22:00:40 +0530

    Time: 0 ms (100%), Space: 16.7 MB (73.28%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
@@ -1,14 +1,31 @@
+// ---------------------- Using Set --------------------------------------------------------
+// class Solution {
+// public:
+//     bool divideArray(vector<int>& nums) {
+//         unordered_set<int> hashSet; // Correct name for the set
+//         for (int num : nums) {
+//             if (hashSet.find(num) != hashSet.end()) { // Check if the element exists in the set
+//                 hashSet.erase(num); // Remove the element
+//             } else {
+//                 hashSet.insert(num); // Add the element
+//             }
+//         }
+//         return hashSet.empty(); // Check if the set is empty
+//     }
+// };
+// ---------------------- Using Set --------------------------------------------------------
+
 class Solution {
 public:
     bool divideArray(vector<int>& nums) {
-        unordered_set<int> hashSet; // Correct name for the set
-        for (int num : nums) {
-            if (hashSet.find(num) != hashSet.end()) { // Check if the element exists in the set
-                hashSet.erase(num); // Remove the element
-            } else {
-                hashSet.insert(num); // Add the element
-            }
+       sort(nums.begin(),nums.end());
+       for(int i=0;i<nums.size();i+=2)
+       {
+        if(nums[i]!=nums[i+1])
+        {
+            return false;
         }
-        return hashSet.empty(); // Check if the set is empty
+       }
+       return true;
     }
 };
