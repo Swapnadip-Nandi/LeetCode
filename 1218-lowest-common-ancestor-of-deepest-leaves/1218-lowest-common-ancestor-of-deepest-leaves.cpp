6e47e0c0d939b0b583eac7e5b8920e23a4728d2a
Swapnadip Nandi <125176423+Swapnadip-Nandi@users.noreply.github.com>
2025-04-04 21:27:18 +0530

    Time: 0 ms (100%), Space: 22 MB (67.37%) - LeetHub

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth=0;
+    TreeNode* lcaDeepestLeaves(TreeNode* root, int* depth=NULL, int level=0) {
+        int d0=0;
+        if (!depth) depth=&d0;
+
+        if (!root) {
+            *depth=level;
+            maxDepth=max(maxDepth, *depth);
+            return root;
+        }
+
+        int lD=0, rD=0;
+        TreeNode* L=lcaDeepestLeaves(root->left, &lD, level+1);
+        TreeNode* R=lcaDeepestLeaves(root->right, &rD, level+1);
+
+        *depth=max(lD, rD);
+        if (lD==rD && lD==maxDepth) return root;
+        return (lD>rD)?L:R;
+    }
+};
