1b90e5de99d075fbc2610bb497724eb5a4e4a8fc
Swapnadip Nandi <125176423+Swapnadip-Nandi@users.noreply.github.com>
2025-04-18 10:52:32 +0530

    Time: 6 ms (34.47%), Space: 9.6 MB (89.27%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,37 +1,27 @@
 class Solution {
 public:
-    using info = pair<int, char>;  // (frequency, digit)
-    string str(const vector<info>& fc) {
-        string ans;
-        for (auto& [f, c] : fc) {
-            ans+=to_string(f);
-            ans.push_back(c);
-        }
-        return ans;
-    }
-
-    vector<info> freq(int n) {
-        if (n==2) return {{1, '1'}};// base case
-        auto fc=freq(n-1);// recursive call
-
-        string s1=str(fc);
-
-        int sz=s1.size();
-        char c=s1[0];
-        vector<info> ans={{1, c}};
-        for (int i=1; i<sz; i++) {
-            if (s1[i]==c) ans.back().first++;
-            else {
-                ans.emplace_back(1, s1[i]);
-                c=s1[i];
-            }
-        }
-        return ans;
-    }
-
     string countAndSay(int n) {
-        if (n==1) return "1";
-        auto fc=freq(n);
-        return str(fc);
+        if (n == 1) return "1";
+
+        string temp = "1";
+
+        for (int i = 2; i <= n; i++) {
+            string curr = "";
+            int count = 1;
+
+            for (int j = 1; j <= temp.size(); j++) {
+                if (j < temp.size() && temp[j] == temp[j - 1]) {
+                    count++;
+                } else {
+                    curr += to_string(count);
+                    curr += temp[j - 1];
+                    count = 1;
+                }
+            }
+
+            temp = curr;
+        }
+
+        return temp;
     }
 };
