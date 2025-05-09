bc9e7e5131bd892d36b7bb683dd48887a3699389
Swapnadip Nandi <125176423+Swapnadip-Nandi@users.noreply.github.com>
2025-05-09 23:38:00 +0530

    Time: 0 ms (100%), Space: 14.1 MB (61.07%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    
+        void dfs(vector<int>& candidates, int target, int start, vector<int>& path, vector<vector<int>>& result) {
+        if (target == 0) {
+            result.push_back(path);
+            return;
+        }
+
+        for (int i = start; i < candidates.size(); i++) {
+            if (i > start && candidates[i] == candidates[i - 1]) continue; 
+            if (candidates[i] > target) break; 
+            path.push_back(candidates[i]);
+            dfs(candidates, target - candidates[i], i + 1, path, result); 
+            path.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end()); 
+        vector<vector<int>> result;
+        vector<int> path;
+        dfs(candidates, target, 0, path, result);
+        return result;
+    }
+    
+    
+};
