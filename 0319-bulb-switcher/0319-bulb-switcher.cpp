7566597b70227bafe21a2c3b7cb1048b72d00b90
Swapnadip Nandi <125176423+Swapnadip-Nandi@users.noreply.github.com>
2025-06-02 12:15:30 +0530

    Time: 0 ms (100%), Space: 7.9 MB (4.17%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -1,7 +1,18 @@
-class Solution {
+class Solution 
+{
 public:
-    int bulbSwitch(int n) {
-        int i = sqrt(n);
-        return i;
+    int bulbSwitch(int n) 
+    {
+    //     int i = sqrt(n);  // Pattern recognize
+    //     return i;
+    // }
+
+
+    int count = 0;
+    
+    for(int i = 1; i*i<=n; i++){
+        count++;
+    }
+    return count;
     }
 };
